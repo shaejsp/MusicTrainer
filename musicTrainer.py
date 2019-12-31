8687a8839d16557da6a797a049ed047d66a4878d
Shae Spallin <shaespallin@gmail.com>
2019-12-31 00:59:12 -0500

    changed red to be less abrasive, moved quit button, added a placeholder for the logo

--- musicTrainer.py
+++ musicTrainer.py
@@ -4,6 +4,8 @@ from random import randint
 """
 TODO:
  - implement tracking for how many they got right
+ - actually record the notes
+ - design logo in photoshop
  - add ability to choose which notes you want to practice
  - add ability to limit to one octave, 3 octaves, 5, or 7 (acts as a multiplier for score)
  - actually implement the sound playing
@@ -17,7 +19,10 @@ global notes
 notes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
 
 global buttonBkg
-buttonBkg = '#c4c4c4'
+buttonBkg = '#e3e3e3'
+
+global redBkg
+redBkg = '#bd3131'
 
 
 class NoteQueue():
@@ -55,22 +60,27 @@ class Application(tk.Frame):
         set up the window
         """
         font = 'Courier 12'
+        blueButton = '#ceddf5'
+        buttonWidth = 10
 
         self.master.title("Music Trainer")
         self.pack(fill='both', expand=1)
 
+        logo = tk.Label(self, text='logo (img) here', font=font, width=22)
+        logo.place(x=20, y=20)
+
         # creates the quit button
-        self.quitButton = tk.Button(self, text='Quit', command=self.quit)
-        self.quitButton.place(x=5, y=5)
+        self.quitButton = tk.Button(self, text='Quit', font=font, width=int(buttonWidth/2), bg=redBkg, command=self.quit)
+        self.quitButton.place(x=620, y=20)
 
         # # creates the play note button
-        self.playButton = tk.Button(self, text='Play Note', font=font, width=10,
+        self.playButton = tk.Button(self, text='Play Note', bg=blueButton, font=font, width=buttonWidth,
                                     height=2, command=self.queue.play)
         self.playButton.place(x=20, y=100)
 
         # creates the play next button
-        self.playNextButton = tk.Button(self, text='Play Next', font=font,
-                                        state='disabled', width=10, height=2,
+        self.playNextButton = tk.Button(self, text='Play Next', bg=blueButton, font=font,
+                                        state='disabled', width=buttonWidth, height=2,
                                         command=self.playNextPressed)
         self.playNextButton.place(x=140, y=100)
 
@@ -90,73 +100,73 @@ class Application(tk.Frame):
         # A button
         self.buttons[notes[0]] = tk.Button(self, text=notes[0], font=font, bg=buttonBkg,
                                            command=lambda: self.noteButtonPressed(notes[0]),
-                                           width=10)
+                                           width=buttonWidth)
         self.buttons[notes[0]].place(x=20, y=180)
 
         # A# button
         self.buttons[notes[1]] = tk.Button(self, text=notes[1], font=font, bg=buttonBkg,
                                            command=lambda: self.noteButtonPressed(notes[1]),
-                                           width=10)
+                                           width=buttonWidth)
         self.buttons[notes[1]].place(x=140, y=180)
 
         # B button
         self.buttons[notes[2]] = tk.Button(self, text=notes[2], font=font, bg=buttonBkg,
                                            command=lambda: self.noteButtonPressed(notes[2]),
-                                           width=10)
+                                           width=buttonWidth)
         self.buttons[notes[2]].place(x=20, y=220)
 
         # C button
         self.buttons[notes[3]] = tk.Button(self, text=notes[3], font=font, bg=buttonBkg,
                                            command=lambda: self.noteButtonPressed(notes[3]),
-                                           width=10)
+                                           width=buttonWidth)
         self.buttons[notes[3]].place(x=20, y=260)
 
         # C# button
         self.buttons[notes[4]] = tk.Button(self, text=notes[4], font=font, bg=buttonBkg,
                                            command=lambda: self.noteButtonPressed(notes[4]),
-                                           width=10)
+                                           width=buttonWidth)
         self.buttons[notes[4]].place(x=140, y=260)
 
         # D button
         self.buttons[notes[5]] = tk.Button(self, text=notes[5], font=font, bg=buttonBkg,
                                            command=lambda: self.noteButtonPressed(notes[5]),
-                                           width=10)
+                                           width=buttonWidth)
         self.buttons[notes[5]].place(x=20, y=300)
 
         # D# button
         self.buttons[notes[6]] = tk.Button(self, text=notes[6], font=font, bg=buttonBkg,
                                            command=lambda: self.noteButtonPressed(notes[6]),
-                                           width=10)
+                                           width=buttonWidth)
         self.buttons[notes[6]].place(x=140, y=300)
 
         # E button
         self.buttons[notes[7]] = tk.Button(self, text=notes[7], font=font, bg=buttonBkg,
                                            command=lambda: self.noteButtonPressed(notes[7]),
-                                           width=10)
+                                           width=buttonWidth)
         self.buttons[notes[7]].place(x=20, y=340)
 
         # F button
         self.buttons[notes[8]] = tk.Button(self, text=notes[8], font=font, bg=buttonBkg,
                                            command=lambda: self.noteButtonPressed(notes[8]),
-                                           width=10)
+                                           width=buttonWidth)
         self.buttons[notes[8]].place(x=20, y=380)
 
         # F# button
         self.buttons[notes[9]] = tk.Button(self, text=notes[9], font=font, bg=buttonBkg,
                                            command=lambda: self.noteButtonPressed(notes[9]),
-                                           width=10)
+                                           width=buttonWidth)
         self.buttons[notes[9]].place(x=140, y=380)
 
         # G button
         self.buttons[notes[10]] = tk.Button(self, text=notes[10], font=font, bg=buttonBkg,
                                             command=lambda: self.noteButtonPressed(notes[10]),
-                                           width=10)
+                                           width=buttonWidth)
         self.buttons[notes[10]].place(x=20, y=420)
 
         # G# button
         self.buttons[notes[11]] = tk.Button(self, text=notes[11], font=font, bg=buttonBkg,
                                             command=lambda: self.noteButtonPressed(notes[11]),
-                                           width=10)
+                                           width=buttonWidth)
         self.buttons[notes[11]].place(x=140, y=420)
 
     def noteButtonPressed(self, buttonNote):
@@ -175,7 +185,7 @@ class Application(tk.Frame):
             self.buttons[buttonNote].config(bg='green', state='normal')
         # else turn the pressed button red
         else:
-            self.buttons[buttonNote].config(bg='red', state='disabled')
+            self.buttons[buttonNote].config(bg=redBkg, state='disabled')
 
     def playNextPressed(self):
         """
